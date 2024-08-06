0f6e99def1af087c8f07a67421f18486e994e573
Ilya Rodin <ilyarodin89@gmail.com>
2024-08-06 18:00:01 +0300

    first commit

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Point point1 = new Point(1, 5);
+        Point point2 = new Point(2, 8);
+        Point point3 = new Point(5, 3);
+        Point point4 = new Point(8, 9);
+
+        //Создается массив точек для создания ломаной линии
+        ArrayList<Point> arrayPoints = new ArrayList<>();
+        arrayPoints.add(point1);
+        arrayPoints.add(point2);
+        arrayPoints.add(point3);
+        arrayPoints.add(point4);
+        arrayPoints.add(point1);
+
+        ClosedPolyLine p1 = new ClosedPolyLine(arrayPoints);
+        double lengthPoly = p1.getLength(arrayPoints);
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "{" + x + ";" + y + "}";
+    }
+}
+
+class PolyLine {
+    ArrayList<Point> listPoint = new ArrayList<>();
+
+    //конструктор с массивом точек
+    public PolyLine(ArrayList<Point> listPoint) {
+        this.listPoint = listPoint;
+    }
+
+    //расчитать длину ломаной
+    public double getLength(ArrayList<Point> listPoint) {
+        this.listPoint = listPoint;
+        double polyLineLength = 0;
+        for (int i = 0; i < listPoint.size(); i++) {
+            if (i == listPoint.size() - 1)
+                break;
+            int katet1 = listPoint.get(i + 1).x - listPoint.get(i).x;
+            int katet2 = listPoint.get(i + 1).y - listPoint.get(i).y;
+            double segment = Math.sqrt(katet1 * katet1 + katet2 * katet2);
+            polyLineLength += segment;
+        }
+        System.out.println("Длина итоговая ломаной " + polyLineLength);
+        return polyLineLength;
+    }
+
+    public String toString() {
+        return "Линия " + listPoint.toString();
+    }
+}
+
+class ClosedPolyLine extends PolyLine {
+    public ClosedPolyLine(ArrayList<Point> listPoint) {
+        super(listPoint);
+    }
+}
